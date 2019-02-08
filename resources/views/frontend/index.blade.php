663410278127dc02fb3e5d2c78d20e561c1e06a1
Csülök <toth.csaba.tamas@gmail.com>
2019-02-08 09:35:14 +0100

    Főképernyő összecsúszás javítása

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -149,15 +149,15 @@
 
 @section('search')
     <!-- SEARCH -->
-    <div class="search-panel">
-        <div class="form-row pt-5 pb-1 flex-row justify-content-center text-white">
-            <h1 class="outline" style="padding-top: 3em;"><strong><i>HazaTér</i></strong></h1>
+    <div class="search-panel pt-lg-5 pt-sm-5 pt-md-5">
+        <div class="form-row pt-lg-5 pt-sm-5 pt-md-5 pb-1 flex-row justify-content-center text-white">
+            <h1 class="outline pt-lg-5"><strong><i>HazaTér</i></strong></h1>
         </div>
         <div class="form-row mt-1 mb-5 flex-row justify-content-center text-white">
             <h4 class="text-center outline">„Lépés-Váltás az Észak-Hegyháton - közös utakon”</h4>
         </div>
 
-        <form id="searchForm" method="POST" action="{{ route('frontend.find') }}" class="form-inline search-form flex-row justify-content-center" autocomplete="off">
+        <form id="searchForm" method="POST" action="{{ route('frontend.find') }}" class="form-inline search-form flex-row justify-content-center p-3" autocomplete="off">
             <div class="form-row mt-5 mb-5">
                 <div class="input-group mr-2 mb-2">
                     <input id="searchStartCity" name="searchStartCity" class="form-control typeahead typeahead-start-city" data-provide="typeahead" type="text" placeholder="Indulási hely" value="{{isset($search)?$search['start_city']:''}}" autocomplete="anyrandomstring">
